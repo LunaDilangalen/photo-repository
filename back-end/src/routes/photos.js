ed6737a87598cf0596d7fe51130ba417ee49b7bd
Datuluna Ali Dilangalen <datuluna.dilangalen@paymaya.com>
2021-10-18 14:46:49 +0800

    updated photos route, initialized POST and PATCH methods

--- back-end/src/routes/photos.js
+++ back-end/src/routes/photos.js
@@ -1,9 +1,14 @@
+const { response } = require('express');
 const express = require('express');
 
 const router = express.Router();
 
 router.post('/', (request, response) => {
-    response.send('We are on photos!')
+    response.send('Creating new Photo!')
+});
+
+router.patch('/', (request, response) => {
+    response.send('Patching Photo...')
 });
 
 module.exports = router;
