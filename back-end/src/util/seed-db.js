323c548ea7a8a16393713cfd8f45c8446237c9fa
Datuluna Ali Dilangalen <datuluna.dilangalen@paymaya.com>
2021-10-18 22:11:36 +0800

    updated seed data, added id

--- back-end/src/util/seed-db.js
+++ back-end/src/util/seed-db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const mongoURI = process.env.DB_LINK
 
 const Photo = require('../models/Photo');
+const { v4: uuidv4 } = require('uuid');
 
 mongoose.connect(mongoURI);
 
@@ -9,6 +10,7 @@ module.exports = function (msg) {
     console.log(msg);
     
     const img1 = new Photo({
+        id: uuidv4(),
         source: 'dummy_url',
         tags: ['seed', 'data'],
         dateModified: Date.now()
@@ -16,6 +18,7 @@ module.exports = function (msg) {
     img1.save().then(() => console.log('Photo saved!'));
     
     const img2 = new Photo({
+        id: uuidv4(),
         source: 'photobucket',
         tags: ['seed', 'data'],
         dateModified: Date.now()
@@ -23,6 +26,7 @@ module.exports = function (msg) {
     img2.save().then(() => console.log('Photo saved!'));
     
     const img3 = new Photo({
+        id: uuidv4(),
         source: 'unsplash',
         tags: ['seed', 'data'],
         dateModified: Date.now()
